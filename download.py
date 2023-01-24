d74bd43e14c30345a20d98e91ae331c640b0c5f2
dr. Konya Sandor <sandorkonya@users.noreply.github.com>
2023-01-24 21:44:56 +0100

    Update download.py

--- download.py
+++ download.py
@@ -3,8 +3,11 @@
 
 # In this example: 
 from paddleocr import PaddleOCR
+import paddle
 
 def download_model():
+    
+    paddle.utils.run_check()
     # do a dry run of loading the huggingface model, which will download weights
     model = PaddleOCR(use_angle_cls=False, lang="en",use_gpu=True)
 
